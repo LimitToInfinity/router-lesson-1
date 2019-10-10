fa707575c2ac651e373af0445cdab7c132fd5b0f
LimitToInfinity <21stLimitless@gmail.com>
2019-10-10 15:49:03 -0600

    Separated from second repo.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
 import Home from './components/Home'
 import About from './components/About'
 import List from './components/List'
 import AppNotFound from './components/AppNotFound'
-
 
 class App extends Component {
   constructor() {
@@ -17,9 +18,21 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        hello
-      </div>
+      <Router>
+        <div className="Nav">
+          <Link className="link" to="/">Home</Link>
+          <Link className="link" to="/about/">About</Link>
+          <Link className="link" to="/lists/">Lists</Link>
+        </div>
+        <Switch>
+          <Route path="/about/" component={ About } />
+          <Route 
+            path="/lists/"
+            render={ (/*props*/)  => <List /*{ ...props }*/ animals={ this.state.list }/> } />
+          <Route exact path="/" component={ Home } />
+          <Route component={ AppNotFound } />
+        </Switch>
+      </Router>
     );
   }
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,31 +1,46 @@
 import React, { Component } from 'react';
+// import { thisExpression } from '@babel/types';
+import { Redirect, withRouter } from "react-router-dom";
 
 class List extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            inHome: false,
         }
     }
 
+    handleRedirectClick = () => {
+        this.setState({inHome: true})
+    }
+
+    handleHistoryPushClick = () => {
+        this.props.history.push("/about")
+    }
 
     render() {
+        
+if (this.state.inHome === true)
+ {
+return <Redirect to = "/" />
+ }        
+        
         // console.log("history props", this.props.history)
 
-        const animalList = this.props.list.list.map((animal, idx) => {
+        const animalList = this.props.animals.map((animal, idx) => {
             return <h1 key={ idx }>{ animal }</h1>
         })
         return (
             <div className="list">
                 { animalList }
 
-                <button>Redirect Component Button</button>
+                <button onClick={ this.handleRedirectClick }>Redirect Component Button</button>
 
-                <button>History Push Button</button>
+                <button onClick={ this.handleHistoryPushClick }>History Push Button</button>
             </div >
 
         );
     }
 }
 
-export default List;
+export default withRouter(List);
